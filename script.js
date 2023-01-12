05701d838ae630cd39c983937f1b0f836947a59e
Mikko <realmiks27@gmail.com>
2023-01-12 10:21:50 -0800

    Create operate function that takes in an operator and 2 numbers and calls the corresponding function with those 2 numbers as parameters

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, num1, num2) {
+    let value;
+    switch (operator) {
+        case '+': 
+            value = add(num1, num2);
+            break;
+        case '-': 
+            value = subtract(num1, num2);
+            break;
+        case '*': 
+            value = multiply(num1, num2);
+            break;
+        case '/': 
+            value = divide(num1, num2);
+            break;
+    }
+    return value;
+}
