698f443a4af1dce7c477b84dc49e3536940c319a
Mikko <realmiks27@gmail.com>
2023-10-29 15:13:44 -0700

    Add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        100: '32rem'
+      }
+    },
   },
   plugins: [],
 }
